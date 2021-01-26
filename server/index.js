93c62060bb7e39ef96cf66bf729125ba4e27fdf9
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-01-26 10:55:13 -0800

    Proxy single service

--- server/index.js
+++ server/index.js
@@ -4,6 +4,16 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello Proxy!')
+  // res.status(400).send('bad kitty')
+})
+
+app.get('/ex', (req, res) => {
+  res.redirect(301, 'http://example.com')
+})
+
+app.get('/calc', (req, res) => {
+  res.redirect(301, 'http://localhost:3001')
+
 })
 
 app.listen(port, () => {
