14533b943a23212a0b4a01fe76462df563fe8827
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-01-26 11:45:44 -0800

    Consume a single service with port hard-coding

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const axios = require('axios')
 
 app.get('/', (req, res) => {
   res.send('Hello Proxy!')
@@ -11,9 +12,17 @@ app.get('/ex', (req, res) => {
   res.redirect(301, 'http://example.com')
 })
 
-app.get('/calc', (req, res) => {
+app.get('/simple_calc', (req, res) => {
   res.redirect(301, 'http://localhost:3001')
+})
 
+app.get('/calc', (req, res) => {
+  axios.get('http://localhost:3001')
+    .then(({data}) => {
+      res.status(200).send(data)
+      console.log('Axios request to localhost:3001 successful')
+    })
+    .catch(err => res.status(400).send('Axios request to localhost:3001 UNsuccessful'))
 })
 
 app.listen(port, () => {
