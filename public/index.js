24fd2d4117400a35b8ee6069d32fb4d1f08c4648
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-01-27 09:48:05 -0800

    Proxy serving up all components

--- public/index.js
+++ public/index.js
@@ -1,46 +1,25 @@
-// const react = require('react')
-// axios req to get /galler endpoint
 
-// react dom reneder
+// // const react = require('react')
+// // axios req to get /galler endpoint
 
-// import axios from 'axios';
-// const axios = require('axios')
-// const doc = document.body;
-const gallery = document.getElementById('gallery');
-const calc = document.getElementById('calculator');
+// // react dom reneder
 
-axios.get('http://localhost:3002')
-  .then(({data}) => {
-    const body = gallery.getElementsByTagName('body')
-    // gallery.innerHTML = data
-    // console.log($.parseHTML(data))
-    // console.log($(data).getElementsByTagName('body'))
-    // console.log(data)
-    // console.log(data.getElementsByTagName('body'))
-    // console.log('Axios request to localhost:3002 (GALLERY) successful')
-    let openI = data.indexOf('<body>')
-    let closeI = data.indexOf('</body>')
-    // let slice = data.slice(openI + 7, ((closeI) - openI))
-    let slice = data.slice(openI+7, closeI)
-    // console.log(slice)
-    let container = document.createElement('div');
-    container.innerHTML = slice;
+// // import axios from 'axios';
+// // const axios = require('axios')
+// // const doc = document.body;
+// const gallery = document.getElementById('gallery');
+// const calc = document.getElementById('calculator');
 
-    // gallery.innerHTML = slice;
-    // gallery.append(slice);
-    gallery.append(container)
-    // container.append(slice)
-    console.log(typeof slice)
-    console.log(typeof gallery)
+// axios.get('http://localhost:3002/bundle')
+//   .then((bundle) => {
+//     console.log(bundle)
+//   })
 
+//   axios.get('http://localhost:3001')
+//   .then(({data}) => {
+//     // calc.innerHTML = data
 
-  })
-
-  axios.get('http://localhost:3001')
-  .then(({data}) => {
-    // calc.innerHTML = data
-
-    // console.log('Axios request to localhost:3002 (GALLERY) successful')
-  })
+//     // console.log('Axios request to localhost:3002 (GALLERY) successful')
+//   })
 
 
--- server/index.js
+++ server/index.js
@@ -1,60 +1,23 @@
 const express = require('express')
-// const cors = require('cors')
-const app = express()
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors')
+
 const port = 3000
-const axios = require('axios')
-
-// const services = require('../services.js');
-// app.use(cors());
+const app = express()
 app.use('/', express.static('public'));
+app.use(cors());
 
-app.get('/', (req, res) => {
-  // res.status(400).send('bad boiiz')
-  // res.send('Hello Proxy!')
+// Gallery
+app.use('/api/photoGallery', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
 
-  // axios req to get bens service
-  // services.gallery
-  // res.status(200).send(services.gallery)
-    // parse out body from html
+// Carousel
+app.use('/api/listings', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
 
-  // axios req to get jasons service
-  // services.calc
-    // parse out body from html
+// Calculator
+app.use('/houses', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
+app.use('/loans', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
+app.use('/taxes', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
 
-  // axios req to get nicks service
-  // services.carousel
-  // res.status(200).send(services.carousel)
-    // parse out body from html
-
-  // build new consolidated body html
-  // send consolidated html as resp
-})
-
-// app.get('/ex', (req, res) => {
-//   res.redirect(301, 'http://example.com')
-// })
-
-// app.get('/simple_calc', (req, res) => {
-//   res.redirect(301, 'http://localhost:3001')
-// })
-
-// app.get('/gallery', (req, res) => {
-//   axios.get('http://localhost:3002')
-//     .then(({data}) => {
-//       res.status(200).send(data)
-//       console.log('Axios request to localhost:3002 (GALLERY) successful')
-//     })
-//     .catch(err => res.status(400).send('Axios request to localhost:3002 (GALLERY) UNsuccessful'))
-// })
-
-// app.get('/calc', (req, res) => {
-//   axios.get('http://localhost:3001')
-//     .then(({data}) => {
-//       res.status(200).send(data)
-//       console.log('Axios request to localhost:3001 (CALC) successful')
-//     })
-//     .catch(err => res.status(400).send('Axios request to localhost:3001 (CALC) UNsuccessful'))
-// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
