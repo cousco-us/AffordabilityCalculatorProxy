8f0d6edb353e4dac6369a1a5be339f4acd022cff
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2021-01-29 10:37:33 -0800

    all service refs changed to production

--- server/index.js
+++ server/index.js
@@ -12,14 +12,14 @@ app.use(cors());
 app.use('/api/photoGallery', createProxyMiddleware({ target: 'http://ec2-13-52-97-136.us-west-1.compute.amazonaws.com:3002/', changeOrigin: true }));
 
 // Carousel
-app.use('/api/listings', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
+app.use('/api/listings', createProxyMiddleware({ target: 'http://ec2-3-129-216-135.us-east-2.compute.amazonaws.com:3003/', changeOrigin: true }));
 
 // Calculator
-app.use('/houses', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
-app.use('/loans', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
-app.use('/taxes', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
+app.use('/houses', createProxyMiddleware({ target: 'http://ec2-52-35-16-109.us-west-2.compute.amazonaws.com:3001/', changeOrigin: true }));
+app.use('/loans', createProxyMiddleware({ target: 'http://ec2-52-35-16-109.us-west-2.compute.amazonaws.com:3001/', changeOrigin: true }));
+app.use('/taxes', createProxyMiddleware({ target: 'http://ec2-52-35-16-109.us-west-2.compute.amazonaws.com:3001/', changeOrigin: true }));
 
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at port ${port}`)
 })
